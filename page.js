6f74c04427d6bdc5ff845d5c6ce81e83887c0956
lucsoft <L.Jank@qualitype.de>
2019-08-28 09:40:13 +0200

    test

--- page.js
+++ page.js
@@ -5,5 +5,5 @@ web.ready = async () => {
     web.func.addBackground("https://data.lucsoft.de/imgs/panda2");
     web.func.addBackground("https://data.lucsoft.de/imgs/panda3");
     web.func.setBackground();
-    web.elements.add().bigTitle({ title: "Hello World", subtitle: "Welcome to WebGen Framework", subtitleposx: "0" })
+    web.elements.add().bigTitle({ title: "Hello World", subtitle: "Welcome to WebGen Framework", subtitleposx: "0" });
 }
