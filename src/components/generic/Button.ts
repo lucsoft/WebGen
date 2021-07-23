7359b49091531cb4b9619b928cefe9f4ebf7577d
lucsoft <mail@lucsoft.de>
2021-07-23 22:57:47 +0200

    Added Dropdown mode to buttons

--- src/components/generic/Button.ts
+++ src/components/generic/Button.ts
@@ -1,61 +1,85 @@
 import { Color } from "../../lib/Color";
-import { Component } from "../../types";
-import { createElement } from "../Components";
+import { ButtonStyle, Component } from "../../types";
+import { createElement, mIcon } from "../Components";
 import '../../css/buttons.webgen.static.css';
 import { loadingWheel } from "../light-components/loadingWheel";
 import { conditionalCSSClass } from "../Helper";
 
-export const enum ButtonState {
-    Inline = "inline",
-    Normal = "normal",
-    Secondary = "secondary",
-    Spinner = "spinner",
-    Progress = "progress"
-}
-
 export type ButtonAction = {
     setProgress: (progress: number) => void
     setEnabled: (enable: boolean) => void
-    changeState: (state: ButtonState) => void
+    changeState: (state: ButtonStyle) => void
 };
-export const Button = ({ state, text, pressOn, progress, style, href }: {
-    state?: ButtonState,
-    style?: Color,
+export const Button = ({ state, text, pressOn, progress, color, href, dropdown, selectedOn }: {
+    state?: ButtonStyle,
+    color?: Color,
     progress?: number,
     href?: string,
-    text: string, pressOn?: (e: ButtonAction) => void
+    dropdown?: ([ displayName: string, action: () => void ])[],
+    text: string,
+    pressOn?: (e: ButtonAction) => void,
+    selectedOn?: () => void
 }): Component => {
     let button = createElement("a") as HTMLAnchorElement;
-    button.classList.add("wbutton", style ?? Color.Grayscaled, state ?? ButtonState.Normal)
+    button.classList.add("wbutton", color ?? Color.Grayscaled, state ?? ButtonStyle.Normal)
     if (href) button.href = href;
     const prog = createElement("div");
     button.append(loadingWheel());
 
+    if (dropdown) {
+        const list = createElement("ul")
+        document.addEventListener('click', (e) => {
+            if (!button.contains(e.target as Node)) {
+                list.classList.remove('open');
+            }
+
+        })
+        dropdown.forEach(([ displayName, action ]) => {
+            const entry = createElement("a")
+            entry.innerText = displayName;
+            entry.onclick = () => {
+                action();
+                selectedOn?.();
+            };
+            list.append(entry)
+        })
+        button.append(list)
+    }
     const setEnabled = (enabled: boolean) => {
         if (enabled)
-            button.classList.replace(Color.Disabled, style ?? Color.Grayscaled)
+            button.classList.replace(Color.Disabled, color ?? Color.Grayscaled)
         else
-            button.classList.replace(style ?? Color.Grayscaled, Color.Disabled)
+            button.classList.replace(color ?? Color.Grayscaled, Color.Disabled)
     }
 
-    const changeState = (state: ButtonState) => {
+    const changeState = (state: ButtonStyle) => {
         button.classList.replace(button.classList[ 2 ], state)
-        conditionalCSSClass(button, state === ButtonState.Spinner, "loading")
+        conditionalCSSClass(button, state === ButtonStyle.Spinner, "loading")
     }
 
     const setProgress = (progValue: number) => {
         if (progValue !== undefined)
             prog.style.width = progValue.toString() + "%";
     }
-    if (state === ButtonState.Spinner) {
+    if (state === ButtonStyle.Spinner) {
         button.classList.add("loading");
     }
-    if (progress !== undefined && state === ButtonState.Progress) {
+    if (progress !== undefined && state === ButtonStyle.Progress) {
         prog.classList.add("progress");
         prog.style.width = progress.toString() + "%";
         button.append(prog);
     }
     button.append(text.toUpperCase());
-    button.onclick = () => pressOn?.({ setProgress, setEnabled, changeState });
+    if (dropdown) {
+        const iconContainer = createElement("div")
+        iconContainer.classList.add("icon-suffix")
+        iconContainer.append(mIcon("expand_more"))
+        button.append(iconContainer);
+    }
+    button.onclick = () => {
+        if (button.classList.contains(Color.Disabled)) return;
+        if (dropdown) button.querySelector('ul')?.classList.toggle("open");
+        pressOn?.({ setProgress, setEnabled, changeState })
+    };
     return button;
 }
