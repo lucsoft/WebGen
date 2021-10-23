871e1c6fff4241c32108c3fb79bd0fa81bd024a6
lucsoft <mail@lucsoft.de>
2021-10-23 13:22:34 +0200

    feat(Helper): Added nullish() which automatically filters components who are nullish

--- src/components/Helper.ts
+++ src/components/Helper.ts
@@ -1,6 +1,10 @@
+import { Component } from "../types"
+
 export const conditionalCSSClass = (element: HTMLElement, condition: boolean | undefined, className: string) => {
     if (condition === true)
         element.classList.add(className)
     else
         element.classList.remove(className)
 }
+
+export const nullish = (components: (Component | null | undefined)[]) => components.filter(x => x) as Component[];
