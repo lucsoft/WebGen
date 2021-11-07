8fe9b9473ef75dc2db0881b08ff31a2aa3626b2e
lucsoft <mail@lucsoft.de>
2021-11-07 12:05:13 +0100

    BREAKING: refactor(components/Button): Removed DropDown of Button to a new DropDown Component

--- src/components/generic/Button.ts
+++ src/components/generic/Button.ts
@@ -1,92 +1,73 @@
 import { Color } from "../../lib/Color";
-import { ButtonStyle, Component } from "../../types";
-import { createElement, draw } from "../Components";
+import { ButtonStyle } from "../../types";
+import { createElement } from "../Components";
 import '../../css/buttons.webgen.static.css';
 import { loadingWheel } from "../light-components/loadingWheel";
-import { conditionalCSSClass } from "../Helper";
+import { changeClassAtIndex, conditionalCSSClass, dropNullish } from "../Helper";
 import { accessibilityButton, accessibilityDisableTabOnDisabled } from "../../lib/Accessibility";
-import { CommonIcon, CommonIconType, Icon } from "./Icon";
 
 export type ButtonAction = {
     setProgress: (progress: number) => void
     setEnabled: (enable: boolean) => void
     changeState: (state: ButtonStyle) => void
 };
-export const Button = ({ state, text, pressOn, progress, color, href, dropdown, selectedOn, arrowDownIcon }: {
-    state?: ButtonStyle,
-    color?: Color,
-    progress?: number,
-    href?: string,
-    dropdown?: ([ displayName: string, action: () => void ])[],
-    text: string,
-    pressOn?: (e: ButtonAction) => void,
-    selectedOn?: () => void,
-    arrowDownIcon?: string
-}): Component => {
-    let button = createElement("a") as HTMLAnchorElement;
-    button.tabIndex = [ ButtonStyle.Spinner, ButtonStyle.Progress ].includes(state ?? ButtonStyle.Normal) ? -1 : accessibilityDisableTabOnDisabled(color);
-    button.classList.add("wbutton", color ?? Color.Grayscaled, state ?? ButtonStyle.Normal)
-    if (href) button.href = href;
+const speicalSyles = [ ButtonStyle.Spinner, ButtonStyle.Progress ];
+const enableTuple = (enabled: boolean, color = Color.Grayscaled) => [ Color.Disabled, color ][ enabled ? "values" : "reverse" ]() as [ Color, Color ];
+
+export const Button = (text: string) => {
+    let button = createElement("a");
+    button.tabIndex = speicalSyles.includes(ButtonStyle.Normal) ? -1 : accessibilityDisableTabOnDisabled();
+    button.classList.add("wbutton", Color.Grayscaled, ButtonStyle.Normal)
     const prog = createElement("div");
     button.append(loadingWheel());
     button.onkeydown = accessibilityButton(button)
-    if (dropdown) {
-        button.classList.add("isList")
-        const list = createElement("ul")
-        document.addEventListener('click', (e) => {
-            if (!button.contains(e.target as Node)) {
-                list.classList.remove('open');
-            }
 
-        })
-        dropdown.forEach(([ displayName, action ]) => {
-            const entry = createElement("a")
-            entry.tabIndex = 0;
-            entry.onkeydown = accessibilityButton(entry);
-            entry.innerText = displayName;
-            entry.onclick = () => {
-                action();
-                selectedOn?.();
-            };
-            list.append(entry)
-        })
-        button.append(list)
-    }
-    const setEnabled = (enabled: boolean) => {
-        if (enabled)
-            button.classList.replace(Color.Disabled, color ?? Color.Grayscaled)
-        else
-            button.classList.replace(color ?? Color.Grayscaled, Color.Disabled)
-    }
-
+    let setEnabled = (enabled: boolean) => button.classList.replace(...enableTuple(enabled))
     const changeState = (state: ButtonStyle) => {
-        button.classList.replace(button.classList[ 2 ], state)
+        changeClassAtIndex(button, state, 2);
         conditionalCSSClass(button, state === ButtonStyle.Spinner, "loading")
     }
 
     const setProgress = (progValue: number) => {
         if (progValue !== undefined)
-            prog.style.width = progValue.toString() + "%";
-    }
-    if (state === ButtonStyle.Spinner) {
-        button.classList.add("loading");
-    }
-    if (progress !== undefined && state === ButtonStyle.Progress) {
-        prog.classList.add("progress");
-        prog.style.width = progress.toString() + "%";
-        button.append(prog);
+            prog.style.width = `${progValue.toString()}%`;
     }
     button.append(text.toUpperCase());
-    if (dropdown) {
-        const iconContainer = createElement("div")
-        iconContainer.classList.add("icon-suffix")
-        iconContainer.append(draw(Icon(arrowDownIcon ?? CommonIcon(CommonIconType.ArrowDown))))
-        button.append(iconContainer);
-    }
+    let pressOn: (e: ButtonAction) => void = () => { };
     button.onclick = () => {
         if (button.classList.contains(Color.Disabled)) return;
-        if (dropdown) button.querySelector('ul')?.classList.toggle("open");
-        pressOn?.({ setProgress, setEnabled, changeState })
+        pressOn({ setProgress, setEnabled, changeState })
     };
-    return button;
+    const settings = {
+        draw: () => button,
+        onClick: (action: ((e: ButtonAction) => void) | string) => {
+            if (typeof action == "string") button.href = action;
+            else pressOn = action;
+            return settings;
+        },
+        setStyle: (style: ButtonStyle, progress?: number) => {
+            button.tabIndex = speicalSyles.includes(style) ? -1 : accessibilityDisableTabOnDisabled();
+            changeClassAtIndex(button, style, 2);
+            if (style === ButtonStyle.Spinner) {
+                button.classList.add("loading");
+            }
+            if (progress !== undefined && style === ButtonStyle.Progress) {
+                prog.classList.add("progress");
+                prog.style.width = `${progress.toString()}%`;
+                button.append(prog);
+            }
+            return settings;
+        },
+        addClass: (...classes: string[]) => {
+            button.classList.add(...classes);
+            return settings;
+        },
+        setColor: (color: Color) => {
+            setEnabled = (enabled: boolean) => button.classList.replace(...enableTuple(enabled, color))
+            button.tabIndex = speicalSyles.includes(button.classList[ 3 ] as ButtonStyle) ? -1 : accessibilityDisableTabOnDisabled(color);
+            changeClassAtIndex(button, color, 1);
+            return settings;
+        }
+    };
+    return settings;
 }
---
+++ src/components/generic/DropDown.ts
@@ -0,0 +1,93 @@
+import { Color } from "../../lib/Color";
+import { ButtonStyle, Component } from "../../types";
+import { createElement } from "../Components";
+import '../../css/buttons.webgen.static.css';
+import { loadingWheel } from "../light-components/loadingWheel";
+import { changeClassAtIndex, conditionalCSSClass } from "../Helper";
+import { accessibilityButton, accessibilityDisableTabOnDisabled } from "../../lib/Accessibility";
+import { CommonIcon, CommonIconType, Icon } from "./Icon";
+import { Custom } from "./Custom";
+
+type ButtonAction = {
+    setProgress: (progress: number) => void
+    setEnabled: (enable: boolean) => void
+    changeState: (state: ButtonStyle) => void
+};
+export const DropDown = ({ state, text, pressOn, progress, color, href, dropdown, selectedOn, arrowDownIcon }: {
+    state?: ButtonStyle,
+    color?: Color,
+    progress?: number,
+    href?: string,
+    dropdown?: ([ displayName: string, action: () => void ])[],
+    text: string,
+    pressOn?: (e: ButtonAction) => void,
+    selectedOn?: () => void,
+    arrowDownIcon?: string
+}): Component => {
+    let button = createElement("a");
+    button.tabIndex = [ ButtonStyle.Spinner, ButtonStyle.Progress ].includes(state ?? ButtonStyle.Normal) ? -1 : accessibilityDisableTabOnDisabled(color);
+    button.classList.add("wbutton", color ?? Color.Grayscaled, state ?? ButtonStyle.Normal)
+    if (href) button.href = href;
+    const prog = createElement("div");
+    button.append(loadingWheel());
+    button.onkeydown = accessibilityButton(button)
+    if (dropdown) {
+        button.classList.add("isList")
+        const list = createElement("ul")
+        document.addEventListener('click', (e) => {
+            if (!button.contains(e.target as Node)) {
+                list.classList.remove('open');
+            }
+
+        })
+        dropdown.forEach(([ displayName, action ]) => {
+            const entry = createElement("a")
+            entry.tabIndex = 0;
+            entry.onkeydown = accessibilityButton(entry);
+            entry.innerText = displayName;
+            entry.onclick = () => {
+                action();
+                selectedOn?.();
+            };
+            list.append(entry)
+        })
+        button.append(list)
+    }
+    const setEnabled = (enabled: boolean) => {
+        if (enabled)
+            button.classList.replace(Color.Disabled, color ?? Color.Grayscaled)
+        else
+            button.classList.replace(color ?? Color.Grayscaled, Color.Disabled)
+    }
+
+    const changeState = (state: ButtonStyle) => {
+        changeClassAtIndex(button, state, 2);
+        conditionalCSSClass(button, state === ButtonStyle.Spinner, "loading")
+    }
+
+    const setProgress = (progValue: number) => {
+        if (progValue !== undefined)
+            prog.style.width = `${progValue.toString()}%`;
+    }
+    if (state === ButtonStyle.Spinner) {
+        button.classList.add("loading");
+    }
+    if (progress !== undefined && state === ButtonStyle.Progress) {
+        prog.classList.add("progress");
+        prog.style.width = `${progress.toString()}%`;
+        button.append(prog);
+    }
+    button.append(text.toUpperCase());
+    if (dropdown) {
+        const iconContainer = createElement("div")
+        iconContainer.classList.add("icon-suffix")
+        iconContainer.append(Icon(arrowDownIcon ?? CommonIcon(CommonIconType.ArrowDown)))
+        button.append(iconContainer);
+    }
+    button.onclick = () => {
+        if (button.classList.contains(Color.Disabled)) return;
+        if (dropdown) button.querySelector('ul')?.classList.toggle("open");
+        pressOn?.({ setProgress, setEnabled, changeState })
+    };
+    return Custom(button);
+}
