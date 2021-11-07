d95a2046cac281916c68ddfd26da4f6ead6b9beb
lucsoft <mail@lucsoft.de>
2021-11-07 12:19:14 +0100

    feature(stacks): Added Support for Spacer. This will removes Align and Justify in Vertical and Horizontal Stacks

--- src/components/generic/Stacks.ts
+++ src/components/generic/Stacks.ts
@@ -2,74 +2,80 @@ import { CommonCard, Component } from "../../types";
 import { createElement } from "../Components";
 import '../../css/stack.webgen.static.css';
 
-export type StackOpts = {
-    gap?: string,
-    classes?: string[]
-    align?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-evenly' | 'space-around',
-    margin?: string
-};
+export function Spacer(): Component {
+    const spacer = createElement("div");
+    spacer.classList.add("spacer");
+    const settings = {
+        draw: () => spacer,
+        addClass: (...classes: string[]) => {
+            spacer.classList.add(...classes);
+            return settings;
+        }
+    };
+    return settings;
+}
 
-export const Horizontal = (opts: StackOpts, ...components: Component[]): Component => {
-    let list = createElement("div") as HTMLDivElement;
+function makeSettings(list: HTMLDivElement) {
+    const settings = {
+        draw: () => list,
+        addClass: (...classes: string[]) => {
+            list.classList.add(...classes);
+            return settings;
+        },
+        setMargin: (margin?: string) => {
+            list.style.width = margin ? `calc(100% - ${margin} - ${margin})` : "";
+            list.style.margin = margin ?? "";
+            return settings;
+        },
+        setGap: (gap: string) => {
+            list.style.gap = gap;
+            return settings;
+        }
+    }
+    return settings;
+}
+
+export const Horizontal = (...components: (Component)[]) => {
+    const list = createElement("div");
     list.classList.add("horizontal-stack");
-    applySettings(opts, list);
-    if (opts.align)
-        list.style.justifyContent = opts.align;
-    components.forEach((e) => {
-        if (e instanceof HTMLElement)
-            list.append(e);
-        else list.append(e.draw())
-    })
-
-    return list;
+    components.forEach((e) => list.append(e.draw()))
+    return makeSettings(list);
 }
 
-export const Vertical = (opts: StackOpts, ...components: Component[]): Component => {
-    let list = createElement("div") as HTMLDivElement;
+export const Vertical = (...components: Component[]) => {
+    let list = createElement("div");
     list.classList.add("vertical-stack");
-    applySettings(opts, list);
-    if (opts.align)
-        list.style.alignItems = opts.align;
-    components.forEach((e) => {
-        if (e instanceof HTMLElement)
-            list.append(e);
-        else list.append(e.draw())
-    })
-
-    return list;
+    components.forEach((e) => list.append(e.draw()))
+    return makeSettings(list);
 }
 
-
-export const Grid = ({ minColumnWidth, maxWidth, gap }: {
-    minColumnWidth?: number,
-    maxWidth?: string,
-    gap?: number
-}, ...cardArray: CommonCard[]): Component => {
-    let element = createElement("grid");
-    if (minColumnWidth)
-        element.style.setProperty('--card-min-width', minColumnWidth + "rem")
-    if (maxWidth)
-        element.style.setProperty('--max-width', maxWidth)
-    if (gap)
-        element.style.setProperty('--gap', minColumnWidth + "rem")
-
+export const Grid = (...cardArray: CommonCard[]) => {
+    let element = createElement("grid" as "title");
     element.append(...cardArray.map(x => {
-        const card = createElement('card');
+        const card = createElement('card' as "div");
         const { height, width } = x.getSize();
         if (height && height > 0) card.style.gridRow = `span ${height}`;
         if (width && width > 0) card.style.gridColumn = `span calc(${width})`;
         return x.draw(card)
     }))
-    return element;
-}
-
-function applySettings(opts: StackOpts, list: HTMLDivElement) {
-    if (opts.gap)
-        list.style.gap = opts.gap;
-    if (opts.classes)
-        list.classList.add(...opts.classes)
-    if (opts.margin) {
-        list.style.width = `calc(100% - ${opts.margin} - ${opts.margin})`;
-        list.style.margin = opts.margin;
-    }
+    const settings = {
+        draw: () => element,
+        addClass: (...classes: string[]) => {
+            element.classList.add(...classes);
+            return settings;
+        },
+        setMaxWidth: (maxWidth: string) => {
+            element.style.setProperty('--max-width', maxWidth)
+            return settings;
+        },
+        setMinColumnWidth: (width: string) => {
+            element.style.setProperty('--card-min-width', width)
+            return settings;
+        },
+        setGap: (gap: string) => {
+            element.style.setProperty('--gap', gap)
+            return settings;
+        }
+    };
+    return settings;
 }
