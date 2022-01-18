1f573c5a4f20c6cb9eae42aa2b17e6545f30af3a
lucsoft <mail@lucsoft.de>
2022-01-18 15:21:26 +0100

    refactor: cleaned up plaintext and stacks with classes

--- src/components/generic/PlainText.ts
+++ src/components/generic/PlainText.ts
@@ -1,15 +1,10 @@
 import { Component } from "../../types.ts";
 import { createElement } from "../Components.ts";
 
-export const PlainText = (title: string, type: `h${1 | 2 | 3 | 4 | 5 | 6}` | `span` = "span") => {
-    const text = createElement(type);
-    text.innerText = title;
-    const settings: Component = {
-        draw: () => text,
-        addClass: (...classes: string[]) => {
-            text.classList.add(...classes);
-            return settings;
-        }
-    };
-    return settings;
+export const PlainText = (title: string, type: `h${1 | 2 | 3 | 4 | 5 | 6}` | `span` = "span") => new class extends Component {
+    wrapper = createElement(type);
+    constructor() {
+        super()
+        this.wrapper.innerText = title;
+    }
 }
--- src/components/generic/Stacks.ts
+++ src/components/generic/Stacks.ts
@@ -1,59 +1,28 @@
-import { BaseComponent, CommonCard, Component } from "../../types.ts";
-import { createElement } from "../Components.ts";
+import { Component } from "../../types.ts";
 import '../../css/stack.webgen.static.css';
 import { dropNullish } from "../Helper.ts";
 
-export function Spacer() {
-    const spacer = createElement("div");
-    spacer.classList.add("spacer");
-    const settings: Component = {
-        draw: () => spacer,
-        addClass: (...classes: string[]) => {
-            spacer.classList.add(...classes);
-            return settings;
-        }
-    };
-    return settings;
-}
-
-export interface StackComponent extends BaseComponent<StackComponent, HTMLDivElement> {
-    setMargin: (margin?: string) => StackComponent
-    setGap: (gap: string) => StackComponent
-}
-function makeSettings(list: HTMLDivElement) {
-    const settings: StackComponent = {
-        draw: () => list,
-        addClass: (...classes: string[]) => {
-            list.classList.add(...classes);
-            return settings;
-        },
-        setMargin: (margin?: string) => {
-            list.style.width = margin ? `calc(100% - ${margin} - ${margin})` : "";
-            list.style.margin = margin ?? "";
-            return settings;
-        },
-        setGap: (gap: string) => {
-            list.style.gap = gap;
-            return settings;
-        }
+class SpacerCompoent extends Component { }
+class AlignComponent extends Component {
+    constructor(type: 'horizontal-stack' | 'vertical-stack', components: (Component | null)[]) {
+        super()
+        this.wrapper.classList.add(type);
+        this.wrapper.append(...dropNullish(...components).map(x => x.draw()))
     }
-    return settings;
+    setMargin(margin?: string): AlignComponent {
+        this.wrapper.style.width = margin ? `calc(100% - ${margin} - ${margin})` : "";
+        this.wrapper.style.margin = margin ?? "";
+        return this;
+    }
+    setGap(gap: string): AlignComponent {
+        this.wrapper.style.gap = gap;
+        return this;
+    }
 }
-
-export const Horizontal = (...components: (Component | null)[]) => {
-    const list = createElement("div");
-    list.classList.add("horizontal-stack");
-    dropNullish(...components).forEach((e) => list.append(e.draw()))
-    return makeSettings(list);
-}
-
-export const Vertical = (...components: (Component | null)[]) => {
-    const list = createElement("div");
-    list.classList.add("vertical-stack");
-    dropNullish(...components).forEach((e) => list.append(e.draw()))
-    return makeSettings(list);
-}
-
+export const Spacer = () => new SpacerCompoent().addClass('spacer');
+export const Horizontal = (...components: (Component | null)[]) => new AlignComponent("horizontal-stack", components);
+export const Vertical = (...components: (Component | null)[]) => new AlignComponent("vertical-stack", components);
+/*
 export interface GridComponent extends BaseComponent<GridComponent, HTMLDivElement> {
     setMaxWidth: (maxWidth: string) => GridComponent
     setMinColumnWidth: (width: string) => GridComponent
@@ -62,7 +31,7 @@ export interface GridComponent extends BaseComponent<GridComponent, HTMLDivEleme
 export const Grid = (...cardArray: CommonCard[]) => {
     const element = createElement("grid" as "div");
     element.append(...cardArray.map(x => {
-        const card = x.make().draw();
+        const card = x.make();
         const { height, width } = x.getSize();
         if (height && height > 0) card.style.gridRow = `span ${height}`;
         if (width && width > 0) card.style.gridColumn = `span calc(${width})`;
@@ -88,4 +57,4 @@ export const Grid = (...cardArray: CommonCard[]) => {
         }
     };
     return settings;
-}
+}*/
