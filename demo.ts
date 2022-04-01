51eaf330245969d7e5efc2af286bd3eecd215250
GitHub <noreply@github.com>
2022-04-01 08:49:21 +0000

    Update dependencies

--- demo.ts
+++ demo.ts
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/x/esbuild@v0.14.29/mod.js';
 import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports@v1.2.3/index.ts";
-import { emptyDirSync } from "https://deno.land/std@0.132.0/fs/mod.ts";
+import { emptyDirSync } from "https://deno.land/std@0.133.0/fs/mod.ts";
 emptyDirSync("dist")
 Deno.copyFileSync("index.html", "dist/index.html")
 console.log("🚀 WebServer @ http://localhost:8100")
