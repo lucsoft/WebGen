badd76dc04e74d3c5f967fc27342657db9617e76
lucsoft <mail@lucsoft.de>
2022-12-12 13:10:10 +0100

    lol

--- src/components/generic/Wizard.ts
+++ src/components/generic/Wizard.ts
@@ -234,7 +234,7 @@ export const Wizard = (settings: WizardSettings, pages: (actions: WizardActions)
 
 function getErrorMessage(state: Partial<{ isValid: validator.SafeParseReturnType<any, any>; }>): string {
     if (!(state.isValid && state.isValid?.success !== true)) return "";
-    const selc = state.isValid.error.errors.find(x => x.code == "custom") ?? state.isValid.error.errors.find(x => x.message != "Required") ?? state.isValid.error.errors[ 0 ];
+    const selc = state.isValid.error.errors.find(x => x.code == "custom" && x.message != "Invalid input") ?? state.isValid.error.errors.find(x => x.message != "Required") ?? state.isValid.error.errors[ 0 ];
 
     // UpperCase and if number box it.
     const path = selc.path.map(x => typeof x == "number" ? `[${x}]` : x.replace(/^./, str => str.toUpperCase())).join("");
