143180b6333cf09ba2a48b08527a842f1336f59e
lucsoft <mail@lucsoft.de>
2022-12-15 22:17:00 +0100

    feat(IconButton): Added Label and asLinkButton

--- src/components/generic/IconButton.ts
+++ src/components/generic/IconButton.ts
@@ -5,21 +5,26 @@ import { CommonIcon, CommonIconType, Icon } from "./Icon.ts";
 import { changeClassAtIndex } from "../Helper.ts";
 import { ButtonStyle, ColoredComponent } from "../../types.ts";
 export class IconButtonComponent extends ColoredComponent {
-    constructor(icon: CommonIconType | string) {
+    constructor(icon: CommonIconType | string, label: string) {
         super();
         this.wrapper.classList.add("wiconbutton", Color.Grayscaled);
         this.wrapper.append(
             Icon(typeof icon == "number" ? CommonIcon(icon) : icon).draw(),
         );
+        this.wrapper.ariaLabel = label;
     }
     setColor(color: Color) {
         this.wrapper.tabIndex = accessibilityDisableTabOnDisabled(color);
         changeClassAtIndex(this.wrapper, color, 1);
         return this;
     }
-    setStyle(_style: ButtonStyle): IconButtonComponent {
+    asLinkButton(url: string) {
+        this.wrapper.href = url;
+        return this;
+    }
+    setStyle(_style: ButtonStyle): this {
         throw new Error("Method not implemented.");
     }
 }
-export const IconButton = (icon: CommonIconType | string) =>
-    new IconButtonComponent(icon);
+export const IconButton = (icon: CommonIconType | string, label: string) =>
+    new IconButtonComponent(icon, label);
