4284bcf5efa3a07d3e0728e6fe74268e91432a67
lucsoft <mail@lucsoft.de>
2023-02-04 14:27:15 +0100

    feat: Added a css tagged template function

---
+++ src/Components.ts
@@ -0,0 +1,26 @@
+/**
+ * Creates a CSSStyleSheet from a tagged templates
+ *
+ * To a WebGen Component:
+ *
+ * ```js
+ * .addStyle(css``);
+ * ```
+ *
+ * Or add styling directly to the html:
+ *
+ * ```js
+ * document.adoptedStyleSheets.push(css`
+ *    .footer {
+ *        gap: 0.5rem;
+ *    }
+ *`);
+ * ```
+ */
+export function css(data: TemplateStringsArray, ...expr: string[]) {
+    const merge = data.map((x, i) => x + (expr[ i ] || ''));
+
+    const style = new CSSStyleSheet();
+    style.replaceSync(merge.join(""));
+    return style;
+}
