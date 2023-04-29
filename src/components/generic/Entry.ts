90e64ccefb9aad7ccff40a801243faaca26eb4ee
lucsoft <mail@lucsoft.de>
2023-04-29 13:35:10 +0200

    feat: Added Entry Component

---
+++ src/components/generic/Entry.ts
@@ -0,0 +1,79 @@
+import { Reactive, State } from "../../State.ts";
+import { Component } from "../../types.ts";
+import { createElement } from "../Components.ts";
+import { loadingWheel } from "../light-components/loadingWheel.ts";
+import { Custom } from "./Custom.ts";
+import { Icon } from "./Icon.ts";
+import { Layer } from "./Layer.ts";
+import { PlainText } from "./PlainText.ts";
+import { Grid, CenterV, Box } from "./Stacks.ts";
+import '../../css/entry.webgen.static.css';
+
+export const Entry = (content: Component | { title: string, subtitle?: string; }) => new class extends Component {
+    prefix = createElement("div");
+    suffix = createElement("div");
+    state = State({
+        isLoading: false
+    });
+
+    constructor() {
+        super();
+        this.wrapper = Layer(content instanceof Component
+            ? content
+            : this.basicContent(content),
+            2,
+            "shadow"
+        ).addClass("wentry").setBorderRadius("large").draw();
+
+        this.prefix.classList.add("prefix");
+        this.wrapper.prepend(this.prefix);
+        this.suffix.classList.add("suffix");
+        this.wrapper.append(this.suffix);
+    }
+
+    private basicContent(content: { title: string, subtitle?: string; }): Component {
+        return Grid(...(content.subtitle
+            ? [
+                PlainText(content.title).addClass("title"),
+                PlainText(content.subtitle).addClass("subtitle")
+            ]
+            : [
+                PlainText(content.title).addClass("title")
+            ])
+        ).addClass("basic-text");
+    }
+
+    override addSuffix(component: Component): this {
+        this.suffix.append(component.draw());
+        return this;
+    }
+
+    override addPrefix(component: Component): this {
+        this.prefix.append(component.draw());
+        this.prefix.style.marginRight = "var(--gap)";
+        return this;
+    }
+    onClick(func: (ev: MouseEvent) => void): this {
+        this.onPromiseClick(async (e) => {
+            await func(e);
+        });
+        return this;
+    }
+
+    onPromiseClick(func: (ev: MouseEvent) => Promise<void>): this {
+        this.wrapper.classList.add("action");
+
+        const item = CenterV(Icon("arrow_forward_ios")).draw();
+        const actionIcon = Reactive(this.state, "isLoading", () =>
+            this.state.isLoading ? Box(Custom(loadingWheel() as Element as HTMLElement)).addClass("loading")
+                : Custom(item)
+        ).addClass("action-item");
+        this.suffix.append(actionIcon.draw());
+
+        this.wrapper.onclick = (ev) => {
+            this.state.isLoading = true;
+            func(ev).then(() => { this.state.isLoading = false; });
+        };
+        return this;
+    }
+};
