de5703e70077dcdf0279b3ca1d7a7768391ee4aa
Skidder <Skidder@bbn.one>
2024-01-13 21:15:32 +0100

    fix: add export for Empty (#31)

--- src/webgen.ts
+++ src/webgen.ts
@@ -17,6 +17,7 @@ export * from './components/Card.ts';
 export * from './components/Checkbox.ts';
 export * from "./components/Content.ts";
 export * from './components/Custom.ts';
+export * from './components/Empty.ts';
 export * from './components/Entry.ts';
 export * from "./components/Flow.ts";
 export * from './components/Form.ts';
