d389a5b1cde0a6af5abde152b36d7682ee610552
Hax <hax@bbn.family>
2024-03-29 16:36:12 +0100

    feat: show real first error message (#35)

--- src/Validate.ts
+++ src/Validate.ts
@@ -2,7 +2,7 @@ import * as zod from 'https://deno.land/x/zod@v3.22.4/mod.ts';
 import { StateData, StateHandler, asRef, listenOnInitalStateKeys } from "./State.ts";
 
 export function getErrorMessage(state: zod.ZodError): string {
-    const selc = state.errors.find(x => x.code == "custom" && x.message != "Invalid input") ?? state.errors.find(x => x.message != "Required") ?? state.errors[ 0 ];
+    const selc = state.errors.find(x => x.message != "Invalid input") ?? state.errors.find(x => x.message != "Required") ?? state.errors[ 0 ];
 
     // UpperCase and if number box it.
     const path = selc.path.map(x => typeof x == "number" ? `[${x}]` : x.replace(/^./, str => str.toUpperCase())).join("");
