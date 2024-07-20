de660511dc120c8c520ae716b9afce5040232f11
lucsoft <mail@lucsoft.de>
2024-07-20 14:19:20 +0200

    Move from jsdeliver to esm.sh for material icons

--- src/icons/MaterialIcons.ts
+++ src/icons/MaterialIcons.ts
@@ -3,11 +3,11 @@ import { Refable, asRef } from "../State.ts";
 import { lazy } from "../lazy.ts";
 
 const iconSet = {
-    outlined: lazy(() => import("https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/outlined.css")),
-    filled: lazy(() => import("https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/filled.css")),
-    round: lazy(() => import("https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/round.css")),
-    sharp: lazy(() => import("https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/sharp.css")),
-    "two-tone": lazy(() => import("https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/two-tone.css")),
+    outlined: lazy(() => import("https://esm.sh/material-icons@1.13.12/iconfont/outlined.css")),
+    filled: lazy(() => import("https://esm.sh/material-icons@1.13.12/iconfont/filled.css")),
+    round: lazy(() => import("https://esm.sh/material-icons@1.13.12/iconfont/round.css")),
+    sharp: lazy(() => import("https://esm.sh/material-icons@1.13.12/iconfont/sharp.css")),
+    "two-tone": lazy(() => import("https://esm.sh/material-icons@1.13.12/iconfont/two-tone.css")),
 };
 export class MaterialIconComponent extends Component {
     constructor(name: Refable<string>, type: Refable<"outlined" | "filled" | "round" | "sharp" | "two-tone">) {
