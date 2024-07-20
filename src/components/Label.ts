2ba6b4b9ce38ebb6bf3a1b095d84c46317131422
lucsoft <mail@lucsoft.de>
2024-07-20 16:54:06 +0200

    cleanup

--- src/components/Label.ts
+++ src/components/Label.ts
@@ -2,17 +2,14 @@ import './Label.css';
 
 import { Component } from '../Component.ts';
 import { createElement } from '../Components.ts';
-import { isRef, Refable } from '../State.ts';
+import { asRef, Refable } from '../State.ts';
 
 export const Label = (title: Refable<string>, type: `h${1 | 2 | 3 | 4 | 5 | 6}` | `span` = "span") => new class extends Component {
     wrapper = createElement(type);
     constructor() {
         super();
         this.addClass("wlabel");
-        if (isRef(title))
-            title.listen((val) => this.wrapper.textContent = val);
-        else
-            this.wrapper.textContent = title;
+        asRef(title).listen((val) => this.wrapper.textContent = val);
     }
 
     setTextAlign(type: "center" | "end" | "start") {
