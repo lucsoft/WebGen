d059056fbe8c74ecd2c227e2ab336151f2d1e247
lucsoft <mail@lucsoft.de>
2024-07-27 20:17:08 +0200

    Allow checkboxes to have a ref

--- src/components/Checkbox.ts
+++ src/components/Checkbox.ts
@@ -1,26 +1,29 @@
 import { accessibilityDisableTabOnDisabled } from "../Accessibility.ts";
 import { Color } from "../Color.ts";
 import { Component } from "../Component.ts";
-import { conditionalCSSClass } from "../Helper.ts";
 import { MIcon } from "../icons/MaterialIcons.ts";
+import { asRef, Refable } from "../State.ts";
 import { ButtonStyle, ColoredComponent } from "../types.ts";
 import './CheckBox.css';
 
 class CheckboxComponent extends ColoredComponent {
+    #selected = asRef(false);
 
-    constructor(selected: boolean, icon: Component) {
+    constructor(selected: Refable<boolean>, icon: Component) {
         super();
         this.wrapper.tabIndex = accessibilityDisableTabOnDisabled();
         this.wrapper.classList.add("wcheckbox");
-        if (selected) this.wrapper.classList.add("selected");
+        asRef(selected).listen((value) => this.#selected.setValue(value));
+        this.addClass(this.#selected.map((v) => v ? "selected" : "unselected"));
         this.wrapper.append(icon.draw());
+        this.wrapper.addEventListener('click', (me) => {
+            this.#selected.setValue(!this.#selected.getValue());
+        });
     }
     onClick(action: (me: MouseEvent, value: boolean) => void) {
         this.wrapper.addEventListener('click', (me) => {
             if (this.wrapper.classList.contains(Color.Disabled)) return;
-            const selected = this.wrapper.classList.contains("selected");
-            conditionalCSSClass(this.wrapper, !selected, "selected");
-            action(me, selected);
+            action(me, this.#selected.getValue());
         });
         return this;
     }
@@ -29,4 +32,4 @@ class CheckboxComponent extends ColoredComponent {
     }
 
 }
-export const Checkbox = (selected = false, icon = MIcon("check")) => new CheckboxComponent(selected, icon);
+export const Checkbox = (selected: Refable<boolean> = false, icon = MIcon("check")) => new CheckboxComponent(selected, icon);
