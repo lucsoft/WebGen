0a4b297b481f89063c67bd8231d20abaa4428afd
lucsoft <mail@lucsoft.de>
2024-07-28 18:33:19 +0200

    Add support for adding classes

--- core/components.ts
+++ core/components.ts
@@ -1,4 +1,5 @@
-import { alwaysRef, asRef, listen, Refable, Reference } from "./state.ts";
+
+import { alwaysRef, asRef, asRefArray, listen, Refable, Reference } from "./state.ts";
 
 // deno-lint-ignore prefer-const
 export let WEBGEN_PREFIX = "wg-";
@@ -16,17 +17,25 @@ export function asWebGenComponent(value: string) {
 
 export class HTMLComponent extends HTMLElement {
     #listener = new Set<{ listen: () => void, unlisten: () => void; }>();
-    textSize = asRef(asRef("") as Reference<string>);
-    protected watch(obj: () => void) {
+    private textSize = asRef(asRef("") as Reference<string>);
+    private cssClassList = asRefArray<string>([]);
+
+    #addWatch(obj: () => { unlisten: () => void; }) {
         let unlisten = () => { };
         this.#listener.add({
             listen: () => {
-                unlisten = listen(obj).unlisten;
+                unlisten = obj().unlisten;
             },
             unlisten: () => {
                 unlisten();
             }
         });
+    }
+    protected useListener<T>(ref: Reference<T>, callback: (newValue: T, oldValue?: T) => void) {
+        this.#addWatch(() => ref.listen(callback));
+    }
+    protected addListen(obj: () => void) {
+        this.#addWatch(() => listen(obj));
     }
     protected connectedCallback() {
         this.#listener.forEach(listener => listener.listen());
@@ -36,14 +45,30 @@ export class HTMLComponent extends HTMLElement {
     }
     constructor() {
         super();
-        this.watch(() => {
-            this.style.fontSize = this.textSize.value.value;
+        this.addListen(() => this.style.fontSize = this.textSize.value.value);
+        this.useListener(this.cssClassList, (newList, oldList) => {
+            const deletedItems = (oldList ?? []).filter(it => !newList.includes(it));
+            const addedItems = (newList ?? []).filter(it => !(oldList ?? []).includes(it));
+            this.classList.remove(...deletedItems);
+            this.classList.add(...addedItems);
         });
     }
     make() {
         const obj = {
             draw: () => this,
-            setTextSize: (value: Refable<string> | string) => { this.textSize.value = alwaysRef(value); return obj; }
+            setTextSize: (value: Refable<string> | string) => { this.textSize.value = alwaysRef(value); return obj; },
+            addClass: <stringy extends string | undefined>(classToken: Refable<stringy>, ...classes: string[]) => {
+                this.classList.add(...classes);
+                const token = alwaysRef(classToken);
+                this.useListener(token, (newValue, oldValue) => {
+                    console.log(newValue, oldValue);
+                    if (oldValue !== undefined && oldValue !== newValue)
+                        this.cssClassList.removeItem(oldValue);
+                    if (newValue !== undefined)
+                        this.cssClassList.addItem(newValue);
+                });
+                return obj;
+            }
         };
         return obj;
     }
