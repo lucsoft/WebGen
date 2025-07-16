9f3de89d60104557e64f736174facc0d30af42d7
Jonas Möller <jonas.moeller23@icloud.com>
2025-07-16 18:10:25 +0200

    improved: styling

--- components/progressbar.ts
+++ components/progressbar.ts
@@ -1,22 +1,34 @@
 import {asWebGenComponent, HTMLComponent} from "../core/components.ts";
 import {alwaysRef, Refable} from "../core/state.ts";
 import {css} from "../core/cssTemplate.ts";
+import {Color} from "../core/color.ts";
+import {Label} from "../core/layout/label.ts";
 
 @asWebGenComponent("progress-bar")
 export class ProgressbarComponent extends HTMLComponent {
+    #outer = document.createElement("div");
+    #inner = document.createElement("div");
+
     constructor(value: Refable<number>) {
         super();
-        this.shadowRoot!.innerHTML = `
-        <div id="outer">
-        <div id="inner"></div></div>
-        `;
+        this.shadowRoot!.appendChild(this.#outer);
+        this.style.height = 20 + "px";
+        this.style.width = "100px";
+        this.#inner.id = "inner";
+        this.#outer.appendChild(this.#inner);
+        this.shadowRoot!.appendChild(Label(value.toString()).draw());
         this.shadowRoot!.adoptedStyleSheets.push(css`
-            div{
-                height: 100px;
-                background-color: aqua;
+            :host {
+                border-radius: 20px;
             }
-            #inner {
-                background-color: green;
+            div{
+                border-radius: 20px;
+                height: ${this.style.height};
+                width: ${this.style.width};
+                background-color: ${Color.neutral.toString()};
+            }
+            #inner{
+                background-color: ${Color.primary.toString()};
             }
         `)
         this.useListener(alwaysRef(value), value => {
